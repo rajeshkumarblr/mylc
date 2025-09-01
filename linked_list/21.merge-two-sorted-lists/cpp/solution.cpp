e88fb65466da3f93017ed4d07c90406b4978d9e9
Rajesh Kumar <rajeshkumar.thandapani@gmail.com>
2025-09-01 07:10:02 +0000

    implemented LC 21 mergelist

--- linked_list/21.merge-two-sorted-lists/cpp/solution.cpp
+++ linked_list/21.merge-two-sorted-lists/cpp/solution.cpp
@@ -24,26 +24,27 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
     {
-        ListNode *head = nullptr, *prev = nullptr;
+        ListNode dummy;
+        dummy.next =  nullptr;
+        ListNode *tail = &dummy;
         while (list1 != nullptr && list2 != nullptr)
         {
-            ListNode *tmp = new ListNode();
             if (list1->val < list2->val) {
-                tmp->val = list1->val;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     tmp->val = list1->val;
+                tail->next = list1;
+                list1 = list1->next;
             } else {
-                tmp->val = list2 ->val;
+                tail->next = list2;
+                list2 = list2->next;
             }
-
-            if (prev) {
-                prev->next = tmp;
-            } else {
-                head = tmp;
-            }
-
-            prev = tmp;
+            tail = tail->next;
         }
 
-        return head;
+        if (list1 != nullptr) {
+            tail->next = list1;
+        } else {
+            tail->next = list2;
+        }
+        return dummy.next;
     }
 };
 // @lc code=end
