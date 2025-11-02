65d51467bcd189d0191ddb79e8d4a891a279f5cf
Rajesh Kumar <rajeshkumar.thandapani@gmail.com>
2025-11-02 12:19:37 +0530

    Fixed the java solution for 160

--- src/java/list/160.intersection-of-two-linked-lists.java
+++ src/java/list/160.intersection-of-two-linked-lists.java
@@ -5,44 +5,16 @@ class P160 {
         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
             if (headA == null || headB == null) return null;
             
-            // Calculate lengths
-            int lenA = getLength(headA);
-            int lenB = getLength(headB);
-            
-            // Align the starting points
             ListNode pA = headA;
             ListNode pB = headB;
             
-            if (lenA > lenB) {
-                for (int i = 0; i < lenA - lenB; i++) {
-                    pA = (pA.Next != null ? pA.Next : pA.next);
-                }
-            } else if (lenB > lenA) {
-                for (int i = 0; i < lenB - lenA; i++) {
-                    pB = (pB.Next != null ? pB.Next : pB.next);
-                }
+            // Two pointer technique: when one reaches end, start from other head
+            while (pA != pB) {
+                pA = (pA == null) ? headB : pA.next;
+                pB = (pB == null) ? headA : pB.next;
             }
             
-            // Find intersection
-            while (pA != null && pB != null) {
-                if (pA == pB) {
-                    return pA;
-                }
-                pA = (pA.Next != null ? pA.Next : pA.next);
-                pB = (pB.Next != null ? pB.Next : pB.next);
-            }
-            
-            return null;
-        }
-        
-        private int getLength(ListNode head) {
-            int length = 0;
-            ListNode curr = head;
-            while (curr != null) {
-                length++;
-                curr = (curr.Next != null ? curr.Next : curr.next);
-            }
-            return length;
+            return pA; // Either intersection node or null
         }
     }
     // @lc code=end
